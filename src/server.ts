5390eb56eb210445a939ab21535b7f6c5cd4bb64
MrGray17 <hammoubelyazid@gmail.com>
2026-08-17 14:25:53 +0100

    Refactored file so that each created server has it's own state

--- src/server.ts
+++ src/server.ts
@@ -2,46 +2,50 @@ import http from "node:http";
 import type { IncomingMessage, ServerResponse } from "node:http";
 import { RateLimiter } from "./limiter.js";
 
-const rateLimiter = new RateLimiter({
-  windowSize: 10_000,
-  requestLimit: 5,
-  clock: () => Date.now(),
-});
-const HANDLER_FUNCTION = (
-  request: IncomingMessage,
-  response: ServerResponse,
-) => {
-  if (request.url == "/hello") {
-    const clientId = request.headers["x-client-id"];
+export const createMyServer = () => {
+  const rateLimiter = new RateLimiter({
+    windowSize: 10_000,
+    requestLimit: 5,
+    clock: () => Date.now(),
+  });
+  const HANDLER_FUNCTION = (
+    request: IncomingMessage,
+    response: ServerResponse,
+  ) => {
+    if (request.url == "/hello") {
+      //we are protection the /hello path
+      const clientId = request.headers["x-client-id"];
 
-    if (clientId == undefined) {
-      response.statusCode = 400;
-      response.end("Bad Request");
-      return;
+      if (clientId == undefined) {
+        response.statusCode = 400;
+        response.end("Bad Request");
+        return;
+      }
+
+      if (Array.isArray(clientId)) {
+        response.statusCode = 400;
+        response.end("Bad Request");
+        return;
+      }
+
+      const allowed = rateLimiter.isAllowed(clientId);
+
+      if (!allowed) {
+        response.statusCode = 429;
+        response.end("Too many requests");
+        return;
+      }
+
+      console.log(request.headers["x-client-id"]);
+      response.end("hello");
+    } else if (request.url == "/status") {
+      response.end("Server is running ...");
+    } else {
+      response.statusCode = 404;
+      response.end("Not found");
     }
-
-    if (Array.isArray(clientId)) {
-      response.statusCode = 400;
-      response.end("Bad Request");
-      return;
-    }
-
-    const allowed = rateLimiter.isAllowed(clientId);
-
-    if (!allowed) {
-      response.statusCode = 429;
-      response.end("Too many requests");
-      return;
-    }
-
-    console.log(request.headers["x-client-id"]);
-    response.end("hello");
-  } else if (request.url == "/status") {
-    response.end("Server is running ...");
-  } else {
-    response.statusCode = 404;
-    response.end("Not found");
-  }
+  };
+  return http.createServer(HANDLER_FUNCTION);
 };
 
-export const server = http.createServer(HANDLER_FUNCTION);
+createMyServer();

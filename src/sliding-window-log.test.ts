5a44ab84f5c14a0424133ae01dd69289ce8aa7ca
El Yazid Hammoubel <hammoubelyazid@gmail.com>
2026-08-26 13:17:19 +0100

    fix sliding window log test imports

--- src/sliding-window-log.test.ts
+++ src/sliding-window-log.test.ts
@@ -1,60 +1,64 @@
 import test from "node:test";
 import assert from "node:assert";
-import { slidingWindowlog } from "./sliding-window-log.js";
+import { SlidingWindowLog } from "./sliding-window-log.js";
 
 test("same-window limit", () => {
-  const swl = new slidingWindowlog({
+  let fakeTime = 0;
+  const swl = new SlidingWindowLog({
     requestLimit: 5,
     windowSize: 10_000,
     clock: () => fakeTime,
   });
-  let fakeTime = 0;
+
   for (let i = 0; i < 4; i++) {
     swl.isAllowed("alice");
   }
+
   assert.strictEqual(swl.isAllowed("alice"), true);
   assert.strictEqual(swl.isAllowed("alice"), false);
 });
 
-test("expired timestamp are removed", () => {
-  const swl = new slidingWindowlog({
+test("expired timestamps are removed", () => {
+  let fakeTime = 0;
+  const swl = new SlidingWindowLog({
     requestLimit: 5,
     windowSize: 10_000,
     clock: () => fakeTime,
   });
-  let fakeTime = 0;
 
   for (let i = 0; i < 5; i++) {
     swl.isAllowed("alice");
   }
+
   assert.strictEqual(swl.isAllowed("alice"), false);
 
   fakeTime = 11_000;
   assert.strictEqual(swl.isAllowed("alice"), true);
 });
 
-test("different users dont interfere", () => {
-  const swl = new slidingWindowlog({
+test("different users do not interfere", () => {
+  let fakeTime = 0;
+  const swl = new SlidingWindowLog({
     requestLimit: 5,
     windowSize: 10_000,
     clock: () => fakeTime,
   });
-  let fakeTime = 0;
 
   for (let i = 0; i < 5; i++) {
     swl.isAllowed("alice");
   }
+
   assert.strictEqual(swl.isAllowed("alice"), false);
   assert.strictEqual(swl.isAllowed("adam"), true);
 });
 
 test("rolling-window boundary behaves correctly", () => {
-  const swl = new slidingWindowlog({
+  let fakeTime = 0;
+  const swl = new SlidingWindowLog({
     requestLimit: 5,
     windowSize: 10_000,
     clock: () => fakeTime,
   });
-  let fakeTime = 0;
 
   swl.isAllowed("alice");
   fakeTime = 3000;
